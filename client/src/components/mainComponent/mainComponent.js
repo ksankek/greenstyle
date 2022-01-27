72b29d9bfb02577cbc4fb5a55c08d5160e31955c
Alexandr Drilenko <ksankek@gmail.com>
2022-01-27 12:22:19 +0300

    v1 front-admin

--- client/src/components/mainComponent/mainComponent.js
+++ client/src/components/mainComponent/mainComponent.js
@@ -1,3 +1,5 @@
+import VueJwtDecode from 'vue-jwt-decode'
+
 export default {
     name: 'mainComponent',
     components: {},
@@ -10,11 +12,17 @@ export default {
                 password: '1234',
                 role: 'USER'
             },
-            users: []
+            users: [],
+            isLogin: false,
+            isAdmin: false,
+            loginForm: {
+                email: 'admin@mail.ru',
+                password: '1234'
+            },
+            activeUser: {}
         }
     },
     created() {
-        this.getUsers()
     },
     mounted() {
     },
@@ -34,23 +42,86 @@ export default {
             }).then(res => {
                 if (res.status === 200) {
                     console.log(res.data)
-                    this.getUsers()
                 }
             })
         },
-        getUsers() {
+        getUsers(id, token) {
             this.$http({
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
                 },
                 url: `http://localhost:5000/api/user`
             }).then(res => {
                 if (res.status === 200) {
+                    this.isAdmin = true
                     this.users = res.data
+                }
+            }).catch(err => {
+                if (err) {
+                    this.getUser(id, token)
+                }
+            })
+        },
+        login() {
+            this.$http({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                url: `http://localhost:5000/api/user/login`,
+                data: JSON.stringify(this.loginForm)
+            }).then(res => {
+                if (res.status === 200) {
+                    this.checkIsAdmin(res.data.token)
+                    this.isLogin = true
+                }
+            })
+        },
+        checkIsAdmin(token) {
+            this.$http({
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                url: `http://localhost:5000/api/user/auth`
+            }).then(res => {
+                if (res.status === 200) {
+                    const {id} = VueJwtDecode.decode(res.data.token);
+                    this.getUsers(id, res.data.token)
+                }
+            })
+        },
+        getUser(id, token) {
+            this.$http({
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                url: `http://localhost:5000/api/user/${id}`
+            }).then(res => {
+                if (res.status === 200) {
+                    this.activeUser = res.data
+                }
+            })
+        },
+        editUser(id, data) {
+            const JSONdata = JSON.stringify(data)
+            this.$http({
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                url: `http://localhost:5000/api/user/${id}`,
+                data: JSONdata
+            }).then(res => {
+                if (res.status === 200) {
+                    console.log(res.data)
                 }
             })
         }
-
     }
 }
