63221f4c816820d562145cd88c6bc50c50f9233b
Alexandr Drilenko <ksankek@gmail.com>
2022-02-05 18:19:18 +0300

    add sportPage

---
+++ client/src/components/pages/sportPage.vue
@@ -0,0 +1,2 @@
+<template src="./sportPage/sportPage.html"></template>
+<script src="./sportPage/sportPage.js" lang="js"></script>
---
+++ client/src/components/pages/sportPage/sportPage.js
@@ -0,0 +1,39 @@
+export default {
+    name: 'SportPage',
+    components: {},
+    props: {
+    },
+    data() {
+        return {
+            articles: []
+        }
+    },
+    created() {
+    },
+    mounted() {
+        this.reqGetAllArticles()
+    },
+    watch: {
+    },
+    computed: {
+    },
+    methods: {
+        reqGetAllArticles() {
+            this.$http({
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                url: `http://localhost:5000/api/article`
+            }).then(res => {
+                if (res.status === 200) {
+                    this.articles = res.data
+                }
+            })
+        },
+
+        getPhoto(url) {
+            return `http://localhost:5000/${url}`
+        }
+    }
+}
